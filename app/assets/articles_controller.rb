d0298aabad61a7b96d578e762c62f6fca1d0140d
c-rosevear <crosevear@infotech.com>
2018-08-23 15:58:58 -0400

    add update and list

--- app/assets/articles_controller.rb
+++ app/assets/articles_controller.rb
@@ -1,11 +1,28 @@
 class ArticlesController < ApplicationController
 
+    def index
+        @list = Article.all
+    end
+
     def new
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = 'Article was successfully updated'
+            redirect_to article_path(@article)
+        else
+            flash[:notice] = 'Article was not updated'
+        end
+    end
+
     def create
-        #render plain:params[:article].inspect
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] = 'Article was successfully created'
@@ -16,7 +33,7 @@ class ArticlesController < ApplicationController
     end
 
     def show
-    @article = Article.find(params[:id])
+        @article = Article.find(params[:id])
     end
 
     private
